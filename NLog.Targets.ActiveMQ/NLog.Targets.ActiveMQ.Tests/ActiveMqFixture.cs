f600e4d26aff2e799796f7799c4cc7b9166d4803
Yurii Sydorets <sidoretsyura@gmail.com>
2023-08-01 15:32:55 +0300

    Added test

---
+++ NLog.Targets.ActiveMQ/NLog.Targets.ActiveMQ.Tests/ActiveMqFixture.cs
@@ -0,0 +1,26 @@
+﻿using DotNet.Testcontainers.Builders;
+
+namespace NLog.Targets.ActiveMQ.Tests;
+
+public class ActiveMqFixture : IAsyncDisposable
+{
+    public readonly AsyncLazy<DotNet.Testcontainers.Containers.IContainer> ActiveMqContainer = new(async () =>
+    {
+        var container = new ContainerBuilder()
+          .WithImage("rmohr/activemq:latest")
+          .WithPortBinding(61616, 61616)
+          .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(61616))
+          .Build();
+        await container.StartAsync();
+        return container;
+    });
+
+    public ActiveMqFixture()
+    {
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await (await ActiveMqContainer).DisposeAsync().ConfigureAwait(false);
+    }
+}
---
+++ NLog.Targets.ActiveMQ/NLog.Targets.ActiveMQ.Tests/ActiveMqTargetIntegrationTest.cs
@@ -0,0 +1,73 @@
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.Util;
+using FluentAssertions;
+using NLog.Common;
+using System.Reflection;
+
+namespace NLog.Targets.ActiveMQ.Tests;
+
+public class ActiveMqTargetIntegrationTest : IClassFixture<ActiveMqFixture>
+{
+    private readonly ActiveMqFixture _activeMqFixture;
+
+    public ActiveMqTargetIntegrationTest(ActiveMqFixture activeMqFixture)
+    {
+        _activeMqFixture = activeMqFixture;
+    }
+
+    [Fact]
+    public async Task Write_SendsMessageToActiveMq()
+    {
+        // Arrange
+        var activeMqContainer = await _activeMqFixture.ActiveMqContainer;
+        var target = new ActiveMqTarget
+        {
+            Uri = $"activemq:tcp://{activeMqContainer.Hostname}:{activeMqContainer.GetMappedPublicPort(61616)}",
+            Destination = "queue://nlog.messages",
+            Layout = "${message}"
+        };
+
+        InitializeTarget(target);
+
+        var logEvent = new LogEventInfo(LogLevel.Info, "TestLogger", "Test Message");
+        var asyncLogEvent = new AsyncLogEventInfo(logEvent, e => { });
+
+        // Act
+        try
+        {
+            target.WriteAsyncLogEvent(asyncLogEvent);
+        }
+        finally
+        {
+            CloseTarget(target);
+        }
+
+        // Assert 
+        var factory = new ConnectionFactory(target.Uri);
+        using (var connection = factory.CreateConnection())
+        using (var session = connection.CreateSession())
+        {
+            var destination = SessionUtil.GetDestination(session, target.Destination.Render(new LogEventInfo()));
+            using (var consumer = session.CreateConsumer(destination))
+            {
+                connection.Start();
+
+                await Task.Delay(1000);
+
+                var receivedMessage = consumer.Receive(new TimeSpan(0, 0, 5)) as ITextMessage;
+                receivedMessage.Should().NotBeNull();
+                logEvent.FormattedMessage.Should().Be(receivedMessage.Text);
+            }
+        }
+    }
+
+    private static void InitializeTarget(Target target) => target.GetType().GetMethod("Initialize", BindingFlags.Instance | BindingFlags.NonPublic)?.Invoke(target, new object?[] { null });
+
+    private static void CloseTarget(Target target) => target.GetType().GetMethod("Close", BindingFlags.Instance | BindingFlags.NonPublic)?.Invoke(target, Array.Empty<object>());
+
+    public ValueTask DisposeAsync()
+    {
+        return _activeMqFixture.DisposeAsync();
+    }
+}
